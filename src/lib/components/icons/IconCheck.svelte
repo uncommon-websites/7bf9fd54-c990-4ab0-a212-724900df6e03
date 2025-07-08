4d029ff06b3dce6ba52588e6d11d0542b82dec7c
Uncle Common <krishnerkar@gmail.com>
2025-07-08 10:21:12 +0000

    chore: preserve manual changes before workflow

---
+++ src/lib/components/icons/IconCheck.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	type Props = {
+		class?: string;
+	};
+	let { class: className = "size-5" }: Props = $props();
+</script>
+
+<svg class={className} fill="none" stroke="currentColor" viewBox="0 0 24 24">
+	<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"/>
+</svg>
